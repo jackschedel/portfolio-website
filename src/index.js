d4648fe87bf85043cda778f626560f1880690c78
Jack Schedel <jackschedel@gmail.com>
2023-05-24 17:18:03 -0400

    temp - typewriter skeleton working

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
+import setupTypewriter from "./scripts/typewriter";
 
+typewriter = setupTypewriter(typewriter);
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
+typewriter.type();
