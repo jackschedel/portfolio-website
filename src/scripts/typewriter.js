1ad43a0621445f8f1d8ac4a415d4324f9c4dba88
Jack Schedel <jackschedel@gmail.com>
2023-07-20 15:53:49 -0400

    fixed scroll added to queue

--- src/scripts/typewriter.js
+++ src/scripts/typewriter.js
@@ -1,80 +1,82 @@
 export default function setupTypewriter(t) {
-    var HTML = t.innerHTML;
+  var HTML = t.innerHTML;
 
-    t.innerHTML = "";
+  t.innerHTML = "";
 
-    var cursorPosition = 0,
-        tag = "",
-        writingTag = false,
-        tagOpen = false,
-        typingSpeed = 75,
+  var cursorPosition = 0,
+    tag = "",
+    writingTag = false,
+    tagOpen = false,
+    typingSpeed = 75,
+    tempTypeSpeed = 0;
+
+  var type = function () {
+    if (writingTag === true) {
+      tag += HTML[cursorPosition];
+    }
+
+    if (HTML[cursorPosition] === "<") {
       tempTypeSpeed = 0;
+      if (tagOpen) {
+        tagOpen = false;
+        writingTag = true;
+      } else {
+        tag = "";
+        tagOpen = true;
+        writingTag = true;
+        tag += HTML[cursorPosition];
+      }
+    }
+    if (!writingTag && tagOpen) {
+      tag.innerHTML += HTML[cursorPosition];
+    }
+    if (!writingTag && !tagOpen) {
+      if (HTML[cursorPosition] === " ") {
+        tempTypeSpeed = 0;
+      } else {
+        tempTypeSpeed = (Math.random() * typingSpeed) + 50;
+      }
+      t.innerHTML += HTML[cursorPosition];
+    }
+    if (writingTag === true && HTML[cursorPosition] === ">") {
+      tempTypeSpeed = (Math.random() * typingSpeed) + 50;
+      writingTag = false;
+      if (tagOpen) {
+        var newSpan = document.createElement("span");
+        t.appendChild(newSpan);
+        newSpan.innerHTML = tag;
+        tag = newSpan.firstChild;
+      }
+    }
 
-    var type = function() {
-      
-        if (writingTag === true) {
-            tag += HTML[cursorPosition];
+    cursorPosition += 1;
+    if (cursorPosition < HTML.length - 1) {
+      setTimeout(type, tempTypeSpeed);
+    } else {
+      document.getElementById("cool-things-hero-btn").style.visibility =
+        "visible";
+      setTimeout(function () {
+        document.getElementById("to-bounce").classList.add("bounce");
+        document.getElementById("to-bounce").classList.remove("down-on-hover");
+      }, 150);
+      var scrollTimeout = setTimeout(function () {
+        if (window.scrollY === 0) {
+          if (window.innerWidth <= 1024) {
+            window.scroll(0, 100);
+          } else {
+            window.scroll(0, 40);
+          }
         }
+      }, 2000);
+      window.addEventListener("scroll", function () {
+        if (window.scrollY !== 0) {
+          clearInterval(scrollTimeout);
+        }
+      });
+    }
+  };
 
-        if (HTML[cursorPosition] === "<") {
-            tempTypeSpeed = 0;
-            if (tagOpen) {
-                tagOpen = false;
-                writingTag = true;
-            } else {
-                tag = "";
-                tagOpen = true;
-                writingTag = true;
-                tag += HTML[cursorPosition];
-            }
-        }
-        if (!writingTag && tagOpen) {
-            tag.innerHTML += HTML[cursorPosition];
-        }
-        if (!writingTag && !tagOpen) {
-            if (HTML[cursorPosition] === " ") {
-                tempTypeSpeed = 0;
-            }
-            else {
-                tempTypeSpeed = (Math.random() * typingSpeed) + 50;
-            }
-            t.innerHTML += HTML[cursorPosition];
-        }
-        if (writingTag === true && HTML[cursorPosition] === ">") {
-            tempTypeSpeed = (Math.random() * typingSpeed) + 50;
-            writingTag = false;
-            if (tagOpen) {
-                var newSpan = document.createElement("span");
-                t.appendChild(newSpan);
-                newSpan.innerHTML = tag;
-                tag = newSpan.firstChild;
-            }
-        }
-
-        cursorPosition += 1;
-        if (cursorPosition < HTML.length - 1) {
-            setTimeout(type, tempTypeSpeed);
-        } else {
-          document.getElementById('cool-things-hero-btn').style.visibility = 'visible';
-          setTimeout(function() {
-            document.getElementById("to-bounce").classList.add("bounce");
-            document.getElementById("to-bounce").classList.remove("down-on-hover");
-          }, 150);
-          setTimeout(function() {
-            if(window.scrollY == 0) {
-              if(window.innerWidth <= 1024){
-                window.scroll(0, 100)
-              } else {
-                window.scroll(0, 40);
-              }
-            } 
-          }, 2000);
-
-        }
-
-    };
-
-    return {
-        type: type
-    };
+  return {
+    type: type,
+  };
 }
